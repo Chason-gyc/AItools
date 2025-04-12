2c0d55f0bcf89b8b892048be678d551a8d9c0d50
guoyachao <yachao.guo@gientech.com>
2025-04-12 23:48:50 +0800

    修改vite.config.js

--- json-tool/vite.config.js
+++ json-tool/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// vite.config.js
-export default {
-  base: '/AItools/', // 注意这里必须是你的 GitHub 仓库名！
-}
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AItools/', // 注意这里必须是你的 GitHub 仓库名！
   plugins: [vue()],
 })
